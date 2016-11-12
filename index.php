759d7b52f4a16e1b69d276496fe5ac6ecd97c919
Towhidul Islam Tuhin <Towhidul Islam Tuhin>
2016-11-12 19:35:24 +0600

    Title change

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require('dbconfig.php');?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Workshop||TUHIN</title>
+        <title>Workshop=TUHIN</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
